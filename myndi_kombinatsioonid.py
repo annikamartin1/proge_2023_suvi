78d4141716f438ddecf8d9459e12d3a74ca8023d
annikamartin1 <annikamartinhm@gmail.com>
2023-06-29 22:06:01 +0300

    29.06.2023

--- myndi_kombinatsioonid.py
+++ myndi_kombinatsioonid.py
@@ -4,16 +4,15 @@ n = int(m[0])
 loppsumma = int(m[1])
 y = sys.stdin.readline().split()
 myndid = [int(a) for a in y]
-kogud = [sys.maxsize] * (loppsumma+1)
-kogud[0] = 0
+kogud = [0] * (loppsumma+1)
+kogud[0] = 1
 for i in range(loppsumma+1):
-    if kogud[i] == sys.maxsize:
+    if kogud[i] == 0:
         continue
     for j in myndid:
         if i+j <= loppsumma:
-            kogud[i+j] = min(kogud[i]+1, kogud[i+j])
-if kogud[loppsumma] == sys.maxsize:
-    print(-1)
-else:
-    print(kogud[loppsumma])
+            kogud[i+j] = (kogud[i] + kogud[i+j]) % 1000000007
+
+print(kogud[loppsumma])
+
 
---
+++ taring_dynaamiline.py
@@ -0,0 +1,7 @@
+import sys
+summa = int(sys.stdin.readline())
+
+visetearv_summas = [sys.maxsize] * (summa + 1)
+visetearv_summas[0] = 1
+
+
