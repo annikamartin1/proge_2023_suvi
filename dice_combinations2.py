9736f48a88c2ba2fdd688eef8aa1516736f011ea
annikamartin1 <annikamartinhm@gmail.com>
2023-07-02 13:37:00 +0300

    dice_combinations uus

--- dice_combinations2.py
+++ dice_combinations2.py
@@ -4,11 +4,9 @@ korrad = [1, 2, 4, 8, 16, 32]
 arv = 0
 eelmine = 63
 for i in range(7, n+1):
-    if i == 7:
-        korrad.append(63)
-        continue
-    arv = (eelmine*2 - korrad[i-8]) % 1000000007
-    korrad.append(arv)
-    eelmine = arv
+    for j in range(6):
+        arv += korrad[i-(j+2)]
+    korrad.append(arv % 1000000007)
+    arv = 0
 tul = int(korrad[n-1])
 print(tul)
