b6aa24dd2d9e09eb7b0a43ce5ca294fa335eff80
annikamartin1 <annikamartinhm@gmail.com>
2023-07-02 13:52:48 +0300

    uuendus

--- dice_combinations2.py
+++ dice_combinations2.py
@@ -1,12 +1,16 @@
 import sys
 n = int(sys.stdin.readline())
-korrad = [1, 2, 4, 8, 16, 32]
+korrad = [0] * (n+1)
+korrad[0] = 1
 arv = 0
 eelmine = 63
-for i in range(7, n+1):
+for i in range(1, n+1):
     for j in range(6):
-        arv += korrad[i-(j+2)]
-    korrad.append(arv % 1000000007)
+        if i-(j+1) < 0:
+            continue
+        else:
+            arv += korrad[i-(j+1)]
+    korrad[i] = (arv % 1000000007)
     arv = 0
-tul = int(korrad[n-1])
+tul = int(korrad[n])
 print(tul)
